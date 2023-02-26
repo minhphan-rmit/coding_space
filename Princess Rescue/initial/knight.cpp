220c12d9abefa47d8ea8a950df81b0b728ba2689
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-02-26 23:09:29 +0700

    add level 1 to 5

--- Princess Rescue/initial/knight.cpp
+++ Princess Rescue/initial/knight.cpp
@@ -24,5 +24,27 @@ int dmg_cal(int & level0, int & opponentCode) {
 }
 
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue) {
+    int route[5] = {1, 2, 9, 18, 99};
+    int routeLength = sizeof(route) / sizeof(int);
+    int maxHP = HP;
+    for (int i = 1; i < (routeLength + 1); i++) {
+        // the event code is 0 then stop immediately and rescue = 1
+        if (route[i] == 0) {
+            rescue = 1;
+            break;
+        }
+        // the event code is 1 to 5:
+        if (route[i] >= 1 && route[i] <= 5) {
+            int currentLevel0 = level0_cal(i);
+            int currentDmg = dmg_cal(currentLevel0, route[i]);
+            if (level > currentLevel0) {
+                level += 1;
+            } else if (level == currentLevel0) {
+                level += 0;
+            } else {
+                HP = HP - currentDmg;
+            }
+        }
+    }
     cout << "Function isn't implemented" << endl;
 }
